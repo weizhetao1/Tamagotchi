f201bbbf17710f821384d7eab680f7244d9a1ebc
William Tao <tao.w@etoncollege.org.uk>
2022-01-19 10:14:32 +0000

    Initial Commit

---
+++ Tamagotchi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tamagotchi
+//
+//  Created by Tao, Weizhe (Coll) on 19/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Tao, Weizhe (Coll) on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
