0397898be68ef1b20df29e1ed23ac0aeeec13247
William Tao <tao.w@etoncollege.org.uk>
2022-01-26 11:21:24 +0000

    Text-based ui

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var tamagotchi = Tamagotchi()
+    let timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack {
-            Text("Pet")
-            HStack {
-                Button("Feed Meal", action: {})
-                Button("Feed Snack", action: {})
-                Button("Play Game", action: {})
+            Text("Age: \(tamagotchi.age)")
+                .onReceive(timer){ _ in
+                    tamagotchi.timeTick()
+                }
+            Text("Hunger: \(tamagotchi.hunger)")
+            Text("Happiness: \(tamagotchi.happiness)")
+            Text("Sickness: \(tamagotchi.sickness)")
+            Text("Excretion: \(tamagotchi.excretion)")
+            Spacer()
+            Text(tamagotchi.pet)
+            Spacer()
+            VStack {
+                Button(action: {tamagotchi.eatMeal()}){
+                    Image("meal")
+                        .resizable()
+                        .frame(width: 100, height: 75)
+                }
+                Button("Feed Snack", action: {
+                    tamagotchi.eatSnack()
+                })
+                Button("Play Game", action: {
+                    tamagotchi.playGame()
+                })
+                Button("Give Medicine", action: {
+                    tamagotchi.giveMedicine()
+                })
+                Button("Clean Up", action: {
+                    tamagotchi.cleanUp()
+                })
             }
         }
     }
--- Tamagotchi/Tamagotchi.swift
+++ Tamagotchi/Tamagotchi.swift
@@ -7,32 +7,67 @@
 
 import Foundation
 
-class Tamagotchi {
-    var hunger: Int = 5{
+class Tamagotchi: ObservableObject {
+    @Published var hunger: Int = 50 {
         didSet{
-            if hunger < 0{
-                hunger = 0
-            }
+            if hunger < 0 {hunger = 0}
+            if hunger > 100 {hunger = 100}
         }
     }
     
-    var age: Int = 0
-    var excretion: Int = 0
-    var sickness: Int = 0
+    @Published var age: Int = 1
+    @Published var excretion: Int = 0 {
+        didSet{
+            if excretion > 100 {excretion = 100}
+        }
+    }
+    @Published var sickness: Int = 0 {
+        didSet{
+            if sickness < 0 {sickness = 0}
+            if sickness > 100 {sickness = 100}
+        }
+    }
+    @Published var happiness: Int = 50 {
+        didSet{
+            if happiness > 100 {happiness = 100}
+            if happiness < 0 {happiness = 0}
+        }
+    }
+    @Published var pet: String = "pet"
     
     func eatSnack() {
-        self.hunger -= 1
+        self.hunger -= 10
     }
     
     func eatMeal() {
-        self.hunger -= 3
+        self.hunger -= 30
     }
     
     func giveMedicine() {
-        self.sickness = Int.random(in: 0...20)
+        self.sickness -= Int.random(in: 20...80)
     }
     
     func cleanUp() {
         self.excretion = 0
+    }
+    
+    func playGame() {
+        self.happiness += Int.random(in: 5...20)
+    }
+    
+    func timeTick() {
+        self.age += 1
+        self.sickness += Int.random(in: 0...2)
+        self.excretion += Int.random(in: 1...3)
+        self.happiness -= Int.random(in: 1...4)
+        self.hunger += Int.random(in: 1...2)
+        if self.age >= 1000{
+            self.pet = "dead"
+        }
+        else if self.hunger == 100 {self.pet = "hungry"}
+        else if self.sickness == 100 {self.pet = "sick"}
+        else if self.excretion == 100 {self.pet = "bathroom"}
+        else if self.happiness == 0 {self.pet = "sad"}
+        else {self.pet = "all happy tamagotchi"}
     }
 }
